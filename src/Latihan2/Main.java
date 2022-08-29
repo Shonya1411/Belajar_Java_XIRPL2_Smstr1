841feff6e574ef3724e6000044421db1c7ed02dd
unknown <shonyadwishafitry@gmail.com>
2022-08-29 14:24:09 +0700

    Initial commit

---
+++ src/Latihan2/Main.java
@@ -0,0 +1,19 @@
+package Latihan2;
+
+public class Main {
+    public static void main(String[] args) {
+        int a=8;
+        int c=12;
+        System.out.println("a + c = " + (a+c));
+        System.out.println("a - c = " + (a-c));
+        System.out.println("a * c = " + (a*c));
+        System.out.println("a / c = " + (a/c));
+        System.out.println("a > c = " + (a>c));
+        System.out.println("a >= c = " + (a>=c));
+        System.out.println("a < c = " + (a<c));
+        System.out.println("a <= c = " + (a<=c));
+        System.out.println("a == c = " + (a==c));
+
+
+    }
+}
